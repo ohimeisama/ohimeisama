d6d080e333a6f5a5334cb33b8399f2cb170b2ee1
mieluo <123456@qq.com>
2018-07-18 16:50:58 +0800

    提交服务器修改

--- src/git/puto.java
+++ src/git/puto.java
@@ -3,6 +3,8 @@ package git;
 public class puto {
 	public static void main(String[] args) {
 		System.out.println("第一个git项目");
+		System.out.println("再次修改");
+		System.out.println("adsasd");
 	}
 
 }
